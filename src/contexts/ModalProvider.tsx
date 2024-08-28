0836e142a24dc3281fefa5235d0ed4a7315c51e7
Sebastien Van Vreckem <sebastien.vanvreckem@gmail.com>
2024-08-28 20:36:31 +0200

    invite people modal

--- src/contexts/ModalProvider.tsx
+++ src/contexts/ModalProvider.tsx
@@ -1,9 +1,11 @@
 import CreateServerModal from '@/components/modals/CreateServerModal';
+import InvitePeopleModal from '@/components/modals/InvitePeopleModal';
 
 const ModalProvider: React.FC = () => {
   return (
     <>
       <CreateServerModal />
+      <InvitePeopleModal />
     </>
   );
 };
