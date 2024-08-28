1ba2cdd2ab80558b3029fa8a71769bf651d5cbc1
Sebastien Van Vreckem <sebastien.vanvreckem@gmail.com>
2024-08-28 17:28:33 +0200

    more dropdown items

--- src/components/server/ServerHeader.tsx
+++ src/components/server/ServerHeader.tsx
@@ -1,10 +1,19 @@
 import { MemberRole } from '@prisma/client';
-import { ChevronDown, UserPlus } from 'lucide-react';
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from 'lucide-react';
 import React from 'react';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 
@@ -30,6 +39,37 @@ const ServerHeader: React.FC<TServerHeaderProps> = ({ server, role }) => {
           <DropdownMenuItem className="text-indingo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
             Invite people
             <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <>
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+              Server Settings
+              <Settings className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+              Manager Members
+              <Users className="h-4 w-4 ml-auto" />
+            </DropdownMenuItem>
+          </>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+            Create Channel
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
