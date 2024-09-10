0009ce66fe55ac18000a2efc2a947ec7c4ff9dad
Sebastien Van Vreckem <sebastien.vanvreckem@gmail.com>
2024-09-10 22:43:30 +0200

    added allowedOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		serverActions: {
+			allowedOrigins: ["discord.sebastienvanvreckem.be", "localhost:3000"]
+		}
+	},
 	images: {
 		remotePatterns: [{ protocol: 'https', hostname: '**' }],
 	},
